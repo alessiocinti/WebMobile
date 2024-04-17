c547e1b229a068f79562e315daac5c9e0d5f4d45
alessiocinti <cloadlibero.it@gmail.com>
2024-04-17 16:40:55 +0200

    Creazione progetto

---
+++ src/main/java/com/unicam/it/IdsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.unicam.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicam/it/IdsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicam.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
